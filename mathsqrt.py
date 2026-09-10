fbcdfaceeecc66c61bcc2af249164f20c595f2a0
JorellUNESA <26051214207@mhs.unesa.ac.id>
2026-09-10 13:13:41 +0700

    akar billangan

---
+++ mathsqrt.py
@@ -0,0 +1,7 @@
+print("\n9.MENCARI AKAR BILANGAN")
+print("-"*30)
+
+bilangan = float(input("Masukkan bilangan: "))
+import math
+akar = math.sqrt(bilangan)
+print(f"Akar dari {bilangan} adalah: {akar:.2f}")
